d82bd5c3f485874eb27a0670987e1dafe64ff989
Nanduni Hansika <67922301+Nanduni96@users.noreply.github.com>
2023-03-21 17:01:12 +0530

    Add files via upload

---
+++ testProject/src/testProject/Main.java
@@ -0,0 +1,37 @@
+package testProject;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static double CalculateMortgagePayment(int P, double r, int n) {
+		double a = Math.pow ((1+r), n);
+		double mortgage = P * ((r*a)/(a -1));
+		return mortgage;
+	}
+
+	public static void main(String[] args) {
+		
+		final byte MONTHS_IN_YEAR = 12;
+		final byte PERCENT = 100;
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Principal: ");
+		int p = scanner.nextInt();
+		
+		System.out.print("Annual interest rate: ");
+		double annualRate = scanner.nextDouble();
+		
+		System.out.print("Period (Years): ");
+		int period = scanner.nextInt();
+		
+		double r = annualRate/(PERCENT * MONTHS_IN_YEAR);
+		int n = period * 12;
+		
+		double MortgagePayment = CalculateMortgagePayment(p,r,n);
+		System.out.println("Mortgage: $"+ MortgagePayment);
+ 
+	}
+
+}
